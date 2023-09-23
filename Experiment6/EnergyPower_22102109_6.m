85fa552ac3be54710d7aa35b48bd79307e29d46a
ATamblingPoder <117522662+ATamblingPoder@users.noreply.github.com>
2023-09-23 23:06:08 +0530

    Fixed Exp 6 - Ques 6

--- Experiment6/EnergyPower_22102109_6.m
+++ Experiment6/EnergyPower_22102109_6.m
@@ -4,5 +4,4 @@ t = -4:1:4;
 xt = abs(sin(pi * t));
 xt = xt .^ 2;
 energy = sum(xt);
-fprintf("The Energy is %e\n", energy);
-fprintf('As Energy is finite, Power is 0\n');
+fprintf("This is a zero signal");
