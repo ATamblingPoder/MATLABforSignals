969f71e3de02848ba8d749b7790624842a3dc43d
ATamblingPoder <117522662+ATamblingPoder@users.noreply.github.com>
2024-01-29 22:06:44 +0530

    Completed q1

--- DigitalSignalProcessing/Experiment03/InLab/q1.m
+++ DigitalSignalProcessing/Experiment03/InLab/q1.m
@@ -1,49 +1,73 @@
 clc;
-clearvars;
-clf;
-n = -2:1:10;
+clear;
+n = -10:1:10;
 
 % ----- (a)
 my_conv = self_conv([1 3 -2 1], [1 1 0 0]);
 inbuilt_conv = conv([1 3 -2 1], [1 1 0 0]);
 my_layout = tiledlayout(3,2);
-nexttile(1, [1,2]);
+title(my_layout, "Q1 -- InLab -- 22102109");
+
+nexttile(1, [1 2]);
 stem([1 3 -2 1], 'filled');
-nexttile(3, [1,2]);
+title("x[n]");
+
+nexttile(3, [1 2]);
 stem([1 1 0 0], 'filled');
+title("h[n]");
+
 nexttile(5);
 stem(my_conv, 'filled');
+title("x[n] * h[n] -- using custom function");
+
 nexttile(6);
 stem(inbuilt_conv, 'filled');
+title("x[n] * h[n] -- using \it conv");
+
 
 
 % ----- (b)
-figure;
 my_conv = self_conv([1 1 1 0 0 0], [0 5 2 2 5 1]);
 inbuilt_conv = conv([1 1 1 0 0 0], [0 5 2 2 5 1]);
-my_layout2 = tiledlayout(3,2);
-nexttile(1, [1,2]);
+my_layout = tiledlayout(3,2);
+title(my_layout, "Q2 -- InLab -- 22102109");
+
+nexttile(1, [1 2]);
 stem([1 1 1 0 0 0], 'filled');
-nexttile(3, [1,2]);
+title("x[n]");
+
+nexttile(3, [1 2]);
 stem([0 5 2 2 5 1], 'filled');
+title("h[n]");
+
 nexttile(5);
 stem(my_conv, 'filled');
+title("x[n] * h[n] -- using custom function");
+
 nexttile(6);
 stem(inbuilt_conv, 'filled');
+title("x[n] * h[n] -- using \it conv");
 
 % ----- (c)
-figure
-my_layout3 = tiledlayout(3,2);
 xn = heaviside(n);
 hn = exp(-n) .* heaviside(n);
 my_conv = self_conv(xn, hn);
 inbuilt_conv = conv(xn, hn);
-nexttile(1, [1,2]);
+my_layout = tiledlayout(3,2);
+title(my_layout, "Q3 -- InLab -- 22102109");
+
+nexttile(1, [1 2]);
 stem(xn, 'filled');
-nexttile(3, [1,2]);
+title("x[n]");
+
+nexttile(3, [1 2]);
 stem(hn, 'filled');
+title("h[n]");
+
 nexttile(5);
 stem(my_conv, 'filled');
+title("x[n] * h[n] -- using custom function");
+
 nexttile(6);
 stem(inbuilt_conv, 'filled');
-clearvars;
+title("x[n] * h[n] -- using \it conv");
